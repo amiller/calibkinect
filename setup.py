2e755b3f4609f7e1a668b9fbfd415bca42e54e94
Andrew Miller <amiller@dappervision.com>
2012-04-15 17:32:00 -0400

    Added cython compiler directive to include embedsignature
    Signed-off-by: Andrew Miller <amiller@dappervision.com>

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ from Cython.Distutils import build_ext
 ext_modules=[Extension("calibkinect.calibkinect_cy",
                        ["calibkinect/calibkinect_cy.pyx"])]
 
+for e in ext_modules:
+    e.pyrex_directives = {"embedsignature": True}
+
 setup(name='CalibKinect',
       version='0.1',
       author='Andrew Miller',
